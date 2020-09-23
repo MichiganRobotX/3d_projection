e274d48ff92ccc0a6bc52d315046408d56fb62f3
Pradeep <pradeep050997@gmail.com>
2020-09-23 15:52:46 -0700

    Add skeleton

--- landmark_detection/scripts/landmark_detection_main.py
+++ landmark_detection/scripts/landmark_detection_main.py
@@ -1,1 +1,28 @@
-#! usr/env/bin/python
+#!/usr/bin/env python
+
+
+# Convert pointcloud to image plane
+def from_lidar_to_image():
+    pass
+
+def search_3d_object_points():
+    pass
+
+def from_lidar_to_map():
+    pass
+
+def get_landmarks():
+    pass
+
+# Subscriber callbacks
+
+if __name__ == '__main__':
+    lidar_to_camera_tf_topic = ""
+    vehicle_to_lidar_tf_topic = ""
+    vechicle_pose_topic = ""
+    camera_intrinsics = None
+    object_detector_topic = "object_detector"
+    bounding_box_topic = "bounding_boxes"
+    lidar_point_cloud_topic = "/scan"
+
+
